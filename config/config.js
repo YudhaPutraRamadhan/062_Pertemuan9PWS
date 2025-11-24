ecd8fd81c70c678f6d7a4393e95cffc0c7e29bcb
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-24 12:01:08 +0900

    Menambahkah controller dan service untuk Komik

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ const test = {
 
 const production = {
   username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_PROD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
